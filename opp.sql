dc612d7d1a3ff98ae04a0c71cb9400bd290895f6
tjovicic-fer <tihomir.jovicic@fer.hr>
2013-12-31 12:40:06 +0100

    Promjenjen encoding u utf8 - croatian

--- opp.sql
+++ opp.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Računalo: 127.0.0.1
--- Vrijeme generiranja: Pro 27, 2013 u 01:33 
+-- Vrijeme generiranja: Pro 31, 2013 u 12:35 
 -- Verzija poslužitelja: 5.6.11
 -- PHP verzija: 5.5.3
 
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `akcijasustava` (
   `idAkcije` int(11) NOT NULL AUTO_INCREMENT,
   `idKorisnika` int(11) NOT NULL,
   `vrijeme` date DEFAULT NULL,
-  `opisAkcije` varchar(100) DEFAULT NULL,
+  `opisAkcije` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
   PRIMARY KEY (`idAkcije`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
@@ -44,12 +44,12 @@ CREATE TABLE IF NOT EXISTS `akcijasustava` (
 
 CREATE TABLE IF NOT EXISTS `alat` (
   `idAlata` int(11) NOT NULL AUTO_INCREMENT,
-  `naziv` varchar(100) DEFAULT NULL,
-  `skraceniNaziv` varchar(100) DEFAULT NULL,
-  `inacica` varchar(100) DEFAULT NULL,
+  `naziv` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
+  `skraceniNaziv` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
+  `inacica` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
   `cijena` decimal(9,2) DEFAULT NULL,
-  `vidljivost` varchar(100) DEFAULT NULL,
-  `link` varchar(100) DEFAULT NULL,
+  `vidljivost` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
+  `link` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
   PRIMARY KEY (`idAlata`),
   UNIQUE KEY `skraceniNaziv` (`skraceniNaziv`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
@@ -62,8 +62,8 @@ CREATE TABLE IF NOT EXISTS `alat` (
 
 CREATE TABLE IF NOT EXISTS `autor` (
   `idAutora` int(11) NOT NULL AUTO_INCREMENT,
-  `ime` varchar(100) DEFAULT NULL,
-  `prezime` varchar(100) DEFAULT NULL,
+  `ime` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
+  `prezime` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
   PRIMARY KEY (`idAutora`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
@@ -75,11 +75,11 @@ CREATE TABLE IF NOT EXISTS `autor` (
 
 CREATE TABLE IF NOT EXISTS `ide` (
   `idIDE` int(11) NOT NULL AUTO_INCREMENT,
-  `naziv` varchar(100) DEFAULT NULL,
-  `skraceniNaziv` varchar(100) NOT NULL,
-  `inacica` varchar(100) DEFAULT NULL,
+  `naziv` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
+  `skraceniNaziv` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci NOT NULL,
+  `inacica` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
   `cijena` decimal(9,2) DEFAULT NULL,
-  `vidljivost` varchar(100) DEFAULT NULL,
+  `vidljivost` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
   PRIMARY KEY (`idIDE`),
   UNIQUE KEY `skraceniNaziv` (`skraceniNaziv`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
@@ -118,7 +118,7 @@ CREATE TABLE IF NOT EXISTS `jeautor` (
 
 CREATE TABLE IF NOT EXISTS `kljucnerijeci` (
   `idTaga` int(11) NOT NULL AUTO_INCREMENT,
-  `tag` varchar(100) DEFAULT NULL,
+  `tag` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
   PRIMARY KEY (`idTaga`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
@@ -130,13 +130,13 @@ CREATE TABLE IF NOT EXISTS `kljucnerijeci` (
 
 CREATE TABLE IF NOT EXISTS `korisnik` (
   `idKorisnika` int(11) NOT NULL AUTO_INCREMENT,
-  `ime` varchar(100) DEFAULT NULL,
-  `prezime` varchar(100) DEFAULT NULL,
-  `mail` varchar(100) DEFAULT NULL,
+  `ime` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
+  `prezime` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
+  `mail` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
   `datumRod` date DEFAULT NULL,
-  `username` varchar(100) DEFAULT NULL,
-  `password` varchar(100) DEFAULT NULL,
-  `vrsta` varchar(100) DEFAULT NULL,
+  `username` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
+  `password` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
+  `vrsta` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
   `validnost` tinyint(1) DEFAULT NULL,
   `uplata` decimal(9,2) DEFAULT NULL,
   PRIMARY KEY (`idKorisnika`)
@@ -176,7 +176,7 @@ CREATE TABLE IF NOT EXISTS `obiljezen` (
 
 CREATE TABLE IF NOT EXISTS `ocjena` (
   `idOcjene` int(11) NOT NULL AUTO_INCREMENT,
-  `oznaka` varchar(100) DEFAULT NULL,
+  `oznaka` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
   `ocjena` decimal(2,1) DEFAULT NULL,
   PRIMARY KEY (`idOcjene`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
@@ -230,8 +230,8 @@ CREATE TABLE IF NOT EXISTS `ostvario` (
 
 CREATE TABLE IF NOT EXISTS `parametar` (
   `idParametra` int(11) NOT NULL AUTO_INCREMENT,
-  `naziv` varchar(100) DEFAULT NULL,
-  `ispitniPrimjer` varchar(100) DEFAULT NULL,
+  `naziv` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
+  `ispitniPrimjer` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
   `iznos` decimal(9,2) DEFAULT NULL,
   PRIMARY KEY (`idParametra`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
@@ -244,12 +244,12 @@ CREATE TABLE IF NOT EXISTS `parametar` (
 
 CREATE TABLE IF NOT EXISTS `platforma` (
   `idPlatforme` int(11) NOT NULL AUTO_INCREMENT,
-  `tip` varchar(100) DEFAULT NULL,
-  `naziv` varchar(100) DEFAULT NULL,
-  `skraceniNaziv` varchar(100) NOT NULL,
-  `inacica` varchar(100) DEFAULT NULL,
-  `link` varchar(100) DEFAULT NULL,
-  `datasheet` varchar(100) DEFAULT NULL,
+  `tip` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
+  `naziv` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
+  `skraceniNaziv` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci NOT NULL,
+  `inacica` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
+  `link` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
+  `datasheet` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
   `cijena` decimal(9,2) DEFAULT NULL,
   PRIMARY KEY (`idPlatforme`),
   UNIQUE KEY `skraceniNaziv` (`skraceniNaziv`)
@@ -313,9 +313,9 @@ CREATE TABLE IF NOT EXISTS `pripadaju` (
 
 CREATE TABLE IF NOT EXISTS `rezultat` (
   `idRezultata` int(11) NOT NULL AUTO_INCREMENT,
-  `naziv` varchar(100) DEFAULT NULL,
+  `naziv` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
   `iznos` decimal(9,2) DEFAULT NULL,
-  `mjernaJedinica` varchar(100) DEFAULT NULL,
+  `mjernaJedinica` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
   PRIMARY KEY (`idRezultata`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
@@ -366,8 +366,8 @@ CREATE TABLE IF NOT EXISTS `sastojiseod` (
 
 CREATE TABLE IF NOT EXISTS `sklopovlje` (
   `idSklopovlja` int(11) NOT NULL AUTO_INCREMENT,
-  `karakteristika` varchar(100) DEFAULT NULL,
-  `skraceniNaziv` varchar(100) DEFAULT NULL,
+  `karakteristika` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
+  `skraceniNaziv` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
   PRIMARY KEY (`idSklopovlja`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
@@ -392,11 +392,11 @@ CREATE TABLE IF NOT EXISTS `uraden` (
 
 CREATE TABLE IF NOT EXISTS `uredaj` (
   `idUredaja` int(11) NOT NULL AUTO_INCREMENT,
-  `karakteristika` varchar(100) DEFAULT NULL,
-  `skraceniNaziv` varchar(100) DEFAULT NULL,
-  `link` varchar(100) DEFAULT NULL,
-  `datasheet` varchar(100) DEFAULT NULL,
-  `vrsta` varchar(100) DEFAULT NULL,
+  `karakteristika` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
+  `skraceniNaziv` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
+  `link` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
+  `datasheet` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
+  `vrsta` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
   PRIMARY KEY (`idUredaja`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
@@ -408,10 +408,10 @@ CREATE TABLE IF NOT EXISTS `uredaj` (
 
 CREATE TABLE IF NOT EXISTS `znanstvenicasopis` (
   `idCasopisa` int(11) NOT NULL AUTO_INCREMENT,
-  `naziv` varchar(100) DEFAULT NULL,
+  `naziv` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
   `godiste` int(11) DEFAULT NULL,
   `redniBroj` int(11) DEFAULT NULL,
-  `adresa` varchar(100) DEFAULT NULL,
+  `adresa` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
   PRIMARY KEY (`idCasopisa`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
@@ -423,10 +423,10 @@ CREATE TABLE IF NOT EXISTS `znanstvenicasopis` (
 
 CREATE TABLE IF NOT EXISTS `znanstvenieksperiment` (
   `idEksperimenta` int(11) NOT NULL AUTO_INCREMENT,
-  `naziv` varchar(100) DEFAULT NULL,
+  `naziv` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
   `vrijemePocetka` date DEFAULT NULL,
   `vrijemeZavrsetka` date DEFAULT NULL,
-  `vidljivost` varchar(100) DEFAULT NULL,
+  `vidljivost` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
   PRIMARY KEY (`idEksperimenta`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
@@ -438,9 +438,9 @@ CREATE TABLE IF NOT EXISTS `znanstvenieksperiment` (
 
 CREATE TABLE IF NOT EXISTS `znanstvenirad` (
   `idRada` int(11) NOT NULL AUTO_INCREMENT,
-  `naslov` varchar(100) DEFAULT NULL,
-  `sazetak` varchar(100) DEFAULT NULL,
-  `lokacija` varchar(100) DEFAULT NULL,
+  `naslov` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
+  `sazetak` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
+  `lokacija` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
   `idCasopisa` int(11) NOT NULL,
   `idSkupa` int(11) NOT NULL,
   PRIMARY KEY (`idRada`),
@@ -456,12 +456,12 @@ CREATE TABLE IF NOT EXISTS `znanstvenirad` (
 
 CREATE TABLE IF NOT EXISTS `znanstveniskup` (
   `idSkupa` int(11) NOT NULL AUTO_INCREMENT,
-  `naziv` varchar(100) DEFAULT NULL,
-  `mjesto` varchar(100) DEFAULT NULL,
-  `drzava` varchar(100) DEFAULT NULL,
+  `naziv` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
+  `mjesto` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
+  `drzava` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
   `danPocetka` date DEFAULT NULL,
   `danZavrsetka` date DEFAULT NULL,
-  `adresa` varchar(100) DEFAULT NULL,
+  `adresa` varchar(100) CHARACTER SET utf8 COLLATE utf8_croatian_ci DEFAULT NULL,
   PRIMARY KEY (`idSkupa`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
@@ -473,107 +473,107 @@ CREATE TABLE IF NOT EXISTS `znanstveniskup` (
 -- Ograničenja za tablicu `ima`
 --
 ALTER TABLE `ima`
-  ADD CONSTRAINT `ima_ibfk_2` FOREIGN KEY (`idEksperimenta`) REFERENCES `znanstvenieksperiment` (`idEksperimenta`),
-  ADD CONSTRAINT `ima_ibfk_1` FOREIGN KEY (`idZapisa`) REFERENCES `portfelj` (`idZapisa`);
+  ADD CONSTRAINT `ima_ibfk_1` FOREIGN KEY (`idZapisa`) REFERENCES `portfelj` (`idZapisa`),
+  ADD CONSTRAINT `ima_ibfk_2` FOREIGN KEY (`idEksperimenta`) REFERENCES `znanstvenieksperiment` (`idEksperimenta`);
 
 --
 -- Ograničenja za tablicu `jeautor`
 --
 ALTER TABLE `jeautor`
-  ADD CONSTRAINT `jeautor_ibfk_2` FOREIGN KEY (`idAutora`) REFERENCES `autor` (`idAutora`),
-  ADD CONSTRAINT `jeautor_ibfk_1` FOREIGN KEY (`idRada`) REFERENCES `znanstvenirad` (`idRada`);
+  ADD CONSTRAINT `jeautor_ibfk_1` FOREIGN KEY (`idRada`) REFERENCES `znanstvenirad` (`idRada`),
+  ADD CONSTRAINT `jeautor_ibfk_2` FOREIGN KEY (`idAutora`) REFERENCES `autor` (`idAutora`);
 
 --
 -- Ograničenja za tablicu `koristi`
 --
 ALTER TABLE `koristi`
-  ADD CONSTRAINT `koristi_ibfk_2` FOREIGN KEY (`idEksperimenta`) REFERENCES `znanstvenieksperiment` (`idEksperimenta`),
-  ADD CONSTRAINT `koristi_ibfk_1` FOREIGN KEY (`idPlatforme`) REFERENCES `platforma` (`idPlatforme`);
+  ADD CONSTRAINT `koristi_ibfk_1` FOREIGN KEY (`idPlatforme`) REFERENCES `platforma` (`idPlatforme`),
+  ADD CONSTRAINT `koristi_ibfk_2` FOREIGN KEY (`idEksperimenta`) REFERENCES `znanstvenieksperiment` (`idEksperimenta`);
 
 --
 -- Ograničenja za tablicu `obiljezen`
 --
 ALTER TABLE `obiljezen`
-  ADD CONSTRAINT `obiljezen_ibfk_2` FOREIGN KEY (`idRada`) REFERENCES `znanstvenirad` (`idRada`),
-  ADD CONSTRAINT `obiljezen_ibfk_1` FOREIGN KEY (`idTaga`) REFERENCES `kljucnerijeci` (`idTaga`);
+  ADD CONSTRAINT `obiljezen_ibfk_1` FOREIGN KEY (`idTaga`) REFERENCES `kljucnerijeci` (`idTaga`),
+  ADD CONSTRAINT `obiljezen_ibfk_2` FOREIGN KEY (`idRada`) REFERENCES `znanstvenirad` (`idRada`);
 
 --
 -- Ograničenja za tablicu `ocjenjuje`
 --
 ALTER TABLE `ocjenjuje`
-  ADD CONSTRAINT `ocjenjuje_ibfk_3` FOREIGN KEY (`idEksperimenta`) REFERENCES `znanstvenieksperiment` (`idEksperimenta`),
   ADD CONSTRAINT `ocjenjuje_ibfk_1` FOREIGN KEY (`idKorisnika`) REFERENCES `korisnik` (`idKorisnika`),
-  ADD CONSTRAINT `ocjenjuje_ibfk_2` FOREIGN KEY (`idOcjene`) REFERENCES `ocjena` (`idOcjene`);
+  ADD CONSTRAINT `ocjenjuje_ibfk_2` FOREIGN KEY (`idOcjene`) REFERENCES `ocjena` (`idOcjene`),
+  ADD CONSTRAINT `ocjenjuje_ibfk_3` FOREIGN KEY (`idEksperimenta`) REFERENCES `znanstvenieksperiment` (`idEksperimenta`);
 
 --
 -- Ograničenja za tablicu `ostvaren`
 --
 ALTER TABLE `ostvaren`
-  ADD CONSTRAINT `ostvaren_ibfk_2` FOREIGN KEY (`idEksperimenta`) REFERENCES `znanstvenieksperiment` (`idEksperimenta`),
-  ADD CONSTRAINT `ostvaren_ibfk_1` FOREIGN KEY (`idAlata`) REFERENCES `alat` (`idAlata`);
+  ADD CONSTRAINT `ostvaren_ibfk_1` FOREIGN KEY (`idAlata`) REFERENCES `alat` (`idAlata`),
+  ADD CONSTRAINT `ostvaren_ibfk_2` FOREIGN KEY (`idEksperimenta`) REFERENCES `znanstvenieksperiment` (`idEksperimenta`);
 
 --
 -- Ograničenja za tablicu `ostvario`
 --
 ALTER TABLE `ostvario`
-  ADD CONSTRAINT `ostvario_ibfk_2` FOREIGN KEY (`idRezultata`) REFERENCES `rezultat` (`idRezultata`),
-  ADD CONSTRAINT `ostvario_ibfk_1` FOREIGN KEY (`idEksperimenta`) REFERENCES `znanstvenieksperiment` (`idEksperimenta`);
+  ADD CONSTRAINT `ostvario_ibfk_1` FOREIGN KEY (`idEksperimenta`) REFERENCES `znanstvenieksperiment` (`idEksperimenta`),
+  ADD CONSTRAINT `ostvario_ibfk_2` FOREIGN KEY (`idRezultata`) REFERENCES `rezultat` (`idRezultata`);
 
 --
 -- Ograničenja za tablicu `posjeduje`
 --
 ALTER TABLE `posjeduje`
-  ADD CONSTRAINT `posjeduje_ibfk_2` FOREIGN KEY (`idZapisa`) REFERENCES `portfelj` (`idZapisa`),
-  ADD CONSTRAINT `posjeduje_ibfk_1` FOREIGN KEY (`idKorisnika`) REFERENCES `korisnik` (`idKorisnika`);
+  ADD CONSTRAINT `posjeduje_ibfk_1` FOREIGN KEY (`idKorisnika`) REFERENCES `korisnik` (`idKorisnika`),
+  ADD CONSTRAINT `posjeduje_ibfk_2` FOREIGN KEY (`idZapisa`) REFERENCES `portfelj` (`idZapisa`);
 
 --
 -- Ograničenja za tablicu `pripada`
 --
 ALTER TABLE `pripada`
-  ADD CONSTRAINT `pripada_ibfk_2` FOREIGN KEY (`idEksperimenta`) REFERENCES `znanstvenieksperiment` (`idEksperimenta`),
-  ADD CONSTRAINT `pripada_ibfk_1` FOREIGN KEY (`idRada`) REFERENCES `znanstvenirad` (`idRada`);
+  ADD CONSTRAINT `pripada_ibfk_1` FOREIGN KEY (`idRada`) REFERENCES `znanstvenirad` (`idRada`),
+  ADD CONSTRAINT `pripada_ibfk_2` FOREIGN KEY (`idEksperimenta`) REFERENCES `znanstvenieksperiment` (`idEksperimenta`);
 
 --
 -- Ograničenja za tablicu `pripadaju`
 --
 ALTER TABLE `pripadaju`
-  ADD CONSTRAINT `pripadaju_ibfk_2` FOREIGN KEY (`idParametra`) REFERENCES `parametar` (`idParametra`),
-  ADD CONSTRAINT `pripadaju_ibfk_1` FOREIGN KEY (`idEksperimenta`) REFERENCES `znanstvenieksperiment` (`idEksperimenta`);
+  ADD CONSTRAINT `pripadaju_ibfk_1` FOREIGN KEY (`idEksperimenta`) REFERENCES `znanstvenieksperiment` (`idEksperimenta`),
+  ADD CONSTRAINT `pripadaju_ibfk_2` FOREIGN KEY (`idParametra`) REFERENCES `parametar` (`idParametra`);
 
 --
 -- Ograničenja za tablicu `sadrzi`
 --
 ALTER TABLE `sadrzi`
-  ADD CONSTRAINT `sadrzi_ibfk_2` FOREIGN KEY (`idZapisa`) REFERENCES `portfelj` (`idZapisa`),
-  ADD CONSTRAINT `sadrzi_ibfk_1` FOREIGN KEY (`idRada`) REFERENCES `znanstvenirad` (`idRada`);
+  ADD CONSTRAINT `sadrzi_ibfk_1` FOREIGN KEY (`idRada`) REFERENCES `znanstvenirad` (`idRada`),
+  ADD CONSTRAINT `sadrzi_ibfk_2` FOREIGN KEY (`idZapisa`) REFERENCES `portfelj` (`idZapisa`);
 
 --
 -- Ograničenja za tablicu `sadrzi_plat_sklop`
 --
 ALTER TABLE `sadrzi_plat_sklop`
-  ADD CONSTRAINT `sadrzi_plat_sklop_ibfk_2` FOREIGN KEY (`idPlatforme`) REFERENCES `platforma` (`idPlatforme`),
-  ADD CONSTRAINT `sadrzi_plat_sklop_ibfk_1` FOREIGN KEY (`idSklopovlja`) REFERENCES `sklopovlje` (`idSklopovlja`);
+  ADD CONSTRAINT `sadrzi_plat_sklop_ibfk_1` FOREIGN KEY (`idSklopovlja`) REFERENCES `sklopovlje` (`idSklopovlja`),
+  ADD CONSTRAINT `sadrzi_plat_sklop_ibfk_2` FOREIGN KEY (`idPlatforme`) REFERENCES `platforma` (`idPlatforme`);
 
 --
 -- Ograničenja za tablicu `sastojiseod`
 --
 ALTER TABLE `sastojiseod`
-  ADD CONSTRAINT `sastojiseod_ibfk_2` FOREIGN KEY (`idUredaja`) REFERENCES `uredaj` (`idUredaja`),
-  ADD CONSTRAINT `sastojiseod_ibfk_1` FOREIGN KEY (`idSklopovlja`) REFERENCES `sklopovlje` (`idSklopovlja`);
+  ADD CONSTRAINT `sastojiseod_ibfk_1` FOREIGN KEY (`idSklopovlja`) REFERENCES `sklopovlje` (`idSklopovlja`),
+  ADD CONSTRAINT `sastojiseod_ibfk_2` FOREIGN KEY (`idUredaja`) REFERENCES `uredaj` (`idUredaja`);
 
 --
 -- Ograničenja za tablicu `uraden`
 --
 ALTER TABLE `uraden`
-  ADD CONSTRAINT `uraden_ibfk_2` FOREIGN KEY (`idEksperimenta`) REFERENCES `znanstvenieksperiment` (`idEksperimenta`),
-  ADD CONSTRAINT `uraden_ibfk_1` FOREIGN KEY (`idIDE`) REFERENCES `ide` (`idIDE`);
+  ADD CONSTRAINT `uraden_ibfk_1` FOREIGN KEY (`idIDE`) REFERENCES `ide` (`idIDE`),
+  ADD CONSTRAINT `uraden_ibfk_2` FOREIGN KEY (`idEksperimenta`) REFERENCES `znanstvenieksperiment` (`idEksperimenta`);
 
 --
 -- Ograničenja za tablicu `znanstvenirad`
 --
 ALTER TABLE `znanstvenirad`
-  ADD CONSTRAINT `znanstvenirad_ibfk_2` FOREIGN KEY (`idSkupa`) REFERENCES `znanstveniskup` (`idSkupa`),
-  ADD CONSTRAINT `znanstvenirad_ibfk_1` FOREIGN KEY (`idCasopisa`) REFERENCES `znanstvenicasopis` (`idCasopisa`);
+  ADD CONSTRAINT `znanstvenirad_ibfk_1` FOREIGN KEY (`idCasopisa`) REFERENCES `znanstvenicasopis` (`idCasopisa`),
+  ADD CONSTRAINT `znanstvenirad_ibfk_2` FOREIGN KEY (`idSkupa`) REFERENCES `znanstveniskup` (`idSkupa`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
