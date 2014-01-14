0a2b8eeeaa56c9ca1ae1c040a8b27eef4fa56daa
bobosmrade <bobodesmra@gmail.com>
2014-01-14 17:46:08 +0100

    Alati
    testirano, s dodanim akcijama sustava, provjeranjem sNaziva, ispravno,
    provuceno kroz code formatter iz fore (zato je diff too large),
    ++NEMOJTE DODAVATI imeSTARO vise++

--- model/DBAlat.php
+++ model/DBAlat.php
@@ -3,20 +3,32 @@
 namespace model;
 use opp\model\AbstractDBModel;
 
-class DBAlat extends AbstractDBModel {
-    public function getTable() {
+class DBAlat extends AbstractDBModel
+{
+    public function getTable()
+    {
         return 'alat';
     }
     
-    public function getPrimaryKeyColumn() {
+    public function getPrimaryKeyColumn()
+    {
         return 'idAlata';
     }
     
-    public function getColumns() {
-        return array('naziv', 'skraceniNaziv', 'inacica', 'cijena', 'vidljivost', 'link');
+    public function getColumns()
+    {
+        return array(
+            'naziv',
+            'skraceniNaziv',
+            'inacica',
+            'cijena',
+            'vidljivost',
+            'link'
+        );
     }
     
-    public function loadSkraceniNaziv($skraceniNaziv) {
+    public function loadSkraceniNaziv($skraceniNaziv)
+    {
         $pov = $this->select()->where(array(
             "skraceniNaziv" => $skraceniNaziv
         ))->fetchAll();
@@ -28,30 +40,48 @@ class DBAlat extends AbstractDBModel {
         }
     }
     
-    public function dohvatiId($skraceniNaziv) {
+    public function ispravanSkraceniNaziv($id, $skraceniNaziv)
+    {
         $pov = $this->select()->where(array(
             "skraceniNaziv" => $skraceniNaziv
         ))->fetchAll();
         
-        if(count($pov)) {
+        foreach ($pov as &$a) {
+            if (($a->skraceniNaziv === $skraceniNaziv) && ($a->idAlata !== $id)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public function dohvatiId($skraceniNaziv)
+    {
+        $pov = $this->select()->where(array(
+            "skraceniNaziv" => $skraceniNaziv
+        ))->fetchAll();
+        
+        if (count($pov)) {
             return $pov[0]->idAlata;
         } else {
             return false;
         }
-            
+        
     }
     
-    public function dohvatiJavneAlate(){
+    public function dohvatiJavneAlate()
+    {
         return $this->select()->where(array(
             "vidljivost" => 'J'
         ))->fetchAll();
-    
+        
     }
     
-    public function brisiJavniAlat($primaryKey) {
+    public function brisiJavniAlat($primaryKey)
+    {
         try {
             $this->load($primaryKey);
-        }  catch (\opp\model\NotFoundException $e) {
+        }
+        catch (\opp\model\NotFoundException $e) {
             return false;
         }
         $this->delete();
--- view/DodavanjeJavnogAlata.php
+++ view/DodavanjeJavnogAlata.php
@@ -3,71 +3,78 @@
 namespace view;
 use opp\view\AbstractView;
 
-class DodavanjeJavnogAlata extends AbstractView {
+class DodavanjeJavnogAlata extends AbstractView
+{
     private $errorMessage;
     
     
-    protected function outputHTML() {
+    protected function outputHTML()
+    {
         echo new components\ErrorMessage(array(
             "errorMessage" => $this->errorMessage
         ));
         
 ?>
-        <p>
-        <form action="<?php echo \route\Route::get("d3")->generate(array(
+       <p>
+        <form action="<?php
+        echo \route\Route::get("d3")->generate(array(
             "controller" => "ekspertnaOsobaCtl",
             "action" => "dodajJavniAlat"
-                ));?>" method="POST">
-				
-			<div class="form-group">
-				<label for="jedin"><b>Naziv</b></label>
-				<input type="text" class="form-control" id="author" placeholder="Upišite naziv" name="naziv" />
-			</div>
-			<br>
-			
-			<div class="form-group">
-				<label for="jedin"><b>Skraćeni naziv</b></label>
-				<input type="text" class="form-control" id="author" placeholder="Upišite skraćeni naziv" name="skraceni" />
-			</div>
-			<br>
-			
-			<div class="form-group">
-				<label for="jedin"><b>Inačica</b></label>
-				<input type="text" class="form-control" id="author" placeholder="Upišite inačicu" name="inacica" />
-			</div>
-			<br>
-			
-			<div class="form-group">
-				<label for="price"><b>Cijena</b></label>
-				<input type="number" min="0" step="0.01" class="form-control" id="price" placeholder="Upišite cijenu" name="cijena" />
-			</div>
-			<br>
-	
+        ));
+?>" method="POST">
+                
+            <div class="form-group">
+                <label for="jedin"><b>Naziv</b></label>
+                <input type="text" class="form-control" id="author" placeholder="Upišite naziv" name="naziv" />
+            </div>
+            <br>
+            
+            <div class="form-group">
+                <label for="jedin"><b>Skraćeni naziv</b></label>
+                <input type="text" class="form-control" id="author" placeholder="Upišite skraćeni naziv" name="skraceniNaziv" />
+            </div>
+            <br>
+            
+            <div class="form-group">
+                <label for="jedin"><b>Inačica</b></label>
+                <input type="text" class="form-control" id="author" placeholder="Upišite inačicu" name="inacica" />
+            </div>
+            <br>
+            
+            <div class="form-group">
+                <label for="price"><b>Cijena</b></label>
+                <input type="number" min="0" step="0.01" class="form-control" id="price" placeholder="Upišite cijenu" name="cijena" />
+            </div>
+            <br>
+    
                         <div class="form-group">
-				<label for="jedin"><b>Poveznica</b></label>
-				<input type="text" class="form-control" id="author" placeholder="Upišite poveznicu" name="link" />
-			</div>
-			<br>
+                <label for="jedin"><b>Poveznica</b></label>
+                <input type="text" class="form-control" id="author" placeholder="Upišite poveznicu" name="link" />
+            </div>
+            <br>
             
-            
+            <br>
             <input type="submit" class="btn btn-primary"  value="Dodaj" />
 
         </form>
         </p>
         
         <br><br>
-		<a href="<?php echo \route\Route::get('d2')->generate(array(
-																"controller" => "korisnik"
-																));?>">
-		<img src="../assets/img/home-icon.jpg" alt="Vrati se u portfelj" height="50" />
+        <a href="<?php
+        echo \route\Route::get('d2')->generate(array(
+            "controller" => "korisnik"
+        ));
+?>">
+        <img src="../assets/img/home-icon.jpg" alt="Vrati se u portfelj" height="50" />
     </a>
 <?php
     }
     
-    public function setErrorMessage($errorMessage) {
+    public function setErrorMessage($errorMessage)
+    {
         $this->errorMessage = $errorMessage;
         return $this;
     }
-
-
+    
+    
 }
